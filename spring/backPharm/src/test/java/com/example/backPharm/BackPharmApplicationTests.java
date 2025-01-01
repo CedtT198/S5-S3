e0ca9926038a4b79b6033c57a3d2830db8d16877
CedtT198 <cedrictiavina426@gmail.com>
2025-01-01 02:51:39 -0500

    all conception done

---
+++ spring/backPharm/src/test/java/com/example/backPharm/BackPharmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backPharm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPharmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
